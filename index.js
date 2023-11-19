46322d4c37ef95311ce48ce3f9a3211928d80df4
GelsonSze <94602747+GelsonSze@users.noreply.github.com>
2023-11-20 02:11:12 +0800

    updated functionalities

--- index.js
+++ index.js
@@ -3,30 +3,22 @@ import cors from 'cors';
 import axios from 'axios'
 import ViteExpress from 'vite-express'
 
-import { fileURLToPath } from "url";
-import { dirname } from "path";
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
 const app = express();
 // ViteExpress.config({ mode: "production" })
 const port = 3001; 
 
 app.use(cors());
-app.use(express.static(__dirname + '/dist'));
 
 app.get('/apiCall', async (req, res) => {
     try {
-        const apiData = await axios.get('https://arbeitnow.com/api/job-board-api');
-
+        var pageNumber = req.query.pageNumber;
+        const apiData = await axios.get(`https://arbeitnow.com/api/job-board-api?page=${pageNumber}`);
+        console.log(apiData.data)
         res.json(apiData.data);
     } catch (e) {
         console.error('Error making API call:', e);
         res.status(500).json({ error: 'Internal Server Error' });
     }
-});
-
-app.get('*', (req, res) => {
-    res.sendFile(__dirname, 'dist/index.html');
 });
 
 ViteExpress.listen(app, port, ()=> console.log(`Vite Express server running on port: ${port}`))
--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,78 @@
 import './App.css'
 import { useEffect, useState } from 'react';
+import parse from 'html-react-parser';
 
 function App() {
+  const [pageNumber, setPageNumber] = useState(1);
   const [data, setData] = useState(null);
+  const [clickedItemDescription, setClickedItemDescription] = useState("No job selected");
+  const [clickedItemUrl, setClickedUrl] = useState(null);
 
   useEffect(() => {
     const fetchApiData = async () => {
       try {
-        const response = await fetch('/apiCall');
+        const response = await fetch(`/apiCall?pageNumber=${pageNumber}`);
         const result = await response.json();
-        setData(result);
+        setData(result.data);
+        console.log("USE EFFECT WAS CALLED")
       } catch (error) {
         console.error('Error fetching data:', error);
       }
     };
 
-    fetchApiData();
-  }, []);
+    fetchApiData(pageNumber);
+  }, [pageNumber]);
+
+  const handlePrevClick = () => {
+    setPageNumber(pageNumber - 1);
+    setData(null)
+  };
+
+  const handleNextClick = () => {
+    setPageNumber(pageNumber + 1);
+    setData(null)
+  };
 
   return (
-    <div>
-      {data ? (
-        <p>Data from the API: {JSON.stringify(data)}</p>
-      ) : (
-        <p>Loading...</p>
-      )}
-    </div>
+    <>
+      <div className="container">
+        <div className="container_div" id= "left_div">
+          {data ? (
+            data.map(item => 
+              <>
+                <div className="job_card" key = {item.slug} onClick={()=> {setClickedItemDescription(item.description); setClickedUrl(item.url)}}>
+                  <p>Company Name: {item.company_name}</p>
+                  <p>Location: {item.location}</p>
+                  <p>Remote: {item.remote ? "Yes": "No"}</p>
+                  <p>Tags: {item.tags}</p>
+                  <p>Title: {item.title}</p>
+                </div>
+                <br></br>
+              </>)
+            ) : (
+            <p>Loading...</p>
+          )}
+        </div>
+
+        <div className="container_div" id="right_div">
+          {clickedItemDescription != "No job selected" ? (
+            <>
+              <div className='description'>{parse(clickedItemDescription)}</div>
+              <p>Learn more information about the job at <a href={clickedItemUrl}>Arbeitnow</a></p>
+            </>
+          ): (
+            <p>No job selected</p>
+          )}
+        </div>
+        <div className='page_directory_div'>
+          {pageNumber > 0 ? (
+              <button id="prev_button" onClick={handlePrevClick}> Prev </button>
+          ): (null)}
+          <button id="current_button">{pageNumber}</button>
+          <button id="next_button" onClick={handleNextClick}> Next </button>
+        </div>
+      </div>
+    </>
   );
 }
 
